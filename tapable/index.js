b2b58ec004e59b22777376285b2b47a320c2df0c
beautyLee <6219023-beautyLee@users.noreply.gitlab.com>
2020-06-20 11:44:13 +0800

    init commit

---
+++ tapable/index.js
@@ -0,0 +1,26 @@
+let {SyncHook} = require('tapable');
+
+class Lesson{
+    constructor(){
+        this.hooks ={
+            arch:new SyncHook(['name'])
+        }
+    }
+    tap(){
+        this.hooks.arch.tap('node',function (name) {
+            console.log('node',name);
+        });
+        this.hooks.arch.tap('react',function (name) {
+            console.log('react',name);
+        })
+    }
+    start(){
+        this.hooks.arch.call('beautyLee');
+    }
+}
+
+var l = new Lesson();
+l.tap();
+l.start();
+
+// 发布订阅者模式
